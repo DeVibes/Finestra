1d757db7da069bf996a56c3672dc76bcda0458d1
Mark <replay.mkp@gmail.com>
2024-04-19 09:18:46 +0300

    created 2 test endpoints

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 
 const DashboardPage = () => {
+  const [serverStatus, setServerStatus] = useState('test');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(response => response.json())
+      .then(data => setServerStatus(data.text))
+  }, []);
+
   return (
-    <div><h1>Dashboard page</h1></div>
+    <div><h1>Dashboard page</h1>{serverStatus}</div>
   )
 }
 
